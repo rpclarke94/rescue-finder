7631733d59da1bfe5b968cc1ffe363fd9842351d
rpclarke94 <ryan@Ryans-MacBook-Pro.local>
2025-09-25 12:38:00 +0100

    Fix ESLint configuration for Next.js deployment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,6 @@
 module.exports = {
-  env: {
-    browser: false,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: [
-    'eslint:recommended',
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  extends: ['next/core-web-vitals'],
   rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': 'off',
-    'prefer-const': 'error',
-    'no-var': 'error',
+    // Add any custom rules here
   },
 };
